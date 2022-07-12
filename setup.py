9a52d5fc290a3ea3e82b1bf0ade9bcf318e5ebaf
svetaStrech <sveta@strech.io>
2022-07-12 11:40:57 +0300

    upload to pypi

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from distutils.core import setup
+setup(
+    name='memphis-py',
+    packages=['memphis-py'],
+    version='0.1',
+    license='GPL',
+    description='A powerful message broker for developers',
+    author='Memphis.dev',
+    author_email='team.memphis.dev',
+    url='https://github.com/memphisdev/memphis.py',
+    download_url='https://github.com/memphisdev/memphis.py/archive/refs/tags/v_0.1.tar.gz',
+    keywords=['message broke', 'devtool', 'streaming', 'data'],
+    install_requires=[
+        'nats-py',
+        'pymitter',
+        'asyncio',
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Dev Tools',
+        'License :: GNU License',
+        'Programming Language :: Python :: +3.7',
+    ],
+)
