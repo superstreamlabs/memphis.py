d6e0859ee6c747b0e96c110b76e25552176687e0
Idan Asulin <74712806+idanasulinmemphis@users.noreply.github.com>
2022-07-12 12:01:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Memphis.dev',
     author_email='team.memphis.dev',
     url='https://github.com/memphisdev/memphis.py',
-    download_url='https://github.com/memphisdev/memphis.py/archive/refs/tags/v_0.1.0.tar.gz',
+    download_url='https://github.com/memphisdev/memphis.py/archive/refs/tags/v0.1.0.tar.gz',
     keywords=['message broke', 'devtool', 'streaming', 'data'],
     install_requires=[
         'nats-py',
