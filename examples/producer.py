08286a59cfaa115234c23aab36323f8cbc62d07b
shohamroditimemphis <shoham@memphis.dev>
2022-08-24 18:35:21 +0300

    revert producer example

--- examples/producer.py
+++ examples/producer.py
@@ -1,12 +1,11 @@
 import asyncio
-
 from memphis import Memphis
 
 
 async def main():
     try:
         memphis = Memphis()
-        await memphis.connect(host="localhost", username="root", connection_token="memphis")
+        await memphis.connect(host="<memphis-host>", username="<application type username>", connection_token="<broker-token>")
 
         producer = memphis.producer(
             station_name="<station-name>", producer_name="<producer-name>")
