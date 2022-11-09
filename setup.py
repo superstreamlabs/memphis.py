de4f9b657a841fd79f2aa906af1a1f9eee0c2207
idanasulinStrech <idan@memphis.dev>
2022-11-09 15:17:52 +0200

    update setup py file

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='memphis-py',
     packages=['memphis'],
-    version='0.2.0',
-    license='GPL',
+    version='0.2.1',
+    license='Apache-2.0',
     description='A powerful messaging platform for modern developers',
     long_description=long_description,
     long_description_content_type='text/markdown',
