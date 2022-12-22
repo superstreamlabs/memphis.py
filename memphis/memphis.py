fb8f360a90fa08e759888f26e8070ae8a2427670
shay23b <shay@memphis.dev>
2022-12-22 11:28:23 +0200

    dlq -> dls

--- memphis/memphis.py
+++ memphis/memphis.py
@@ -625,7 +625,7 @@ class Consumer:
         """Consume events.
         """
         self.t_consume = asyncio.create_task(self.__consume(callback))
-        self.t_dlq = asyncio.create_task(self.__consume_dlq(callback))
+        self.t_dls = asyncio.create_task(self.__consume_dls(callback))
 
     async def __consume(self, callback):
         subject = get_internal_name(self.station_name)
@@ -652,13 +652,13 @@ class Consumer:
             else:
                 break
 
-    async def __consume_dlq(self, callback):
+    async def __consume_dls(self, callback):
         subject = get_internal_name(self.station_name)
         consumer_group = get_internal_name(self.consumer_group)
         try:
-            subscription_name = "$memphis_dlq_"+subject+"_"+consumer_group
-            self.consumer_dlq = await self.connection.broker_manager.subscribe(subscription_name, subscription_name)
-            async for msg in self.consumer_dlq.messages:
+            subscription_name = "$memphis_dls_"+subject+"_"+consumer_group
+            self.consumer_dls = await self.connection.broker_manager.subscribe(subscription_name, subscription_name)
+            async for msg in self.consumer_dls.messages:
                 await callback([Message(msg, self.connection, self.consumer_group)], None)
         except Exception as e:
             print("dls", e)
@@ -678,7 +678,7 @@ class Consumer:
         """Destroy the consumer.
         """
         self.t_consume.cancel()
-        self.t_dlq.cancel()
+        self.t_dls.cancel()
         self.t_ping.cancel()
         self.pull_interval_ms = None
         try:
