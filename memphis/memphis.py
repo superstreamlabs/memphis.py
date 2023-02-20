56b5efaa6c068b9a4114c174d895fec75acf1ae8
idanasulinStrech <idan@memphis.dev>
2023-02-20 22:31:33 +0200

    version update

--- memphis/memphis.py
+++ memphis/memphis.py
@@ -1,3 +1,18 @@
+# Credit for The NATS.IO Authors
+# Copyright 2021-2022 The Memphis Authors
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
 import asyncio
 import json
 import random
@@ -16,20 +31,6 @@ from graphql import build_schema as build_graphql_schema
 from graphql import parse as parse_graphql
 from graphql import validate as validate_graphql
 from jsonschema import validate
-
-# Credit for The NATS.IO Authors
-# Copyright 2021-2022 The Memphis Authors
-# Licensed under the Apache License, Version 2.0 (the “License”);
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 
 schemaVFailAlertType = "schema_validation_fail_alert"
