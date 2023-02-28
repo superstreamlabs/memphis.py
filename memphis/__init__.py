4d25dc9bfd8a8aa9e9de215c85fb152f2ab7e755
Alon David <37884564+alon-david@users.noreply.github.com>
2023-02-28 12:59:51 +0200

    refactor storage and retention variables to be enum based (#118)

--- memphis/__init__.py
+++ memphis/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import memphis.retention_types
-import memphis.storage_types
 from memphis.memphis import (
     Headers,
     Memphis,
