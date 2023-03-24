8bb89f9e6bf8ac226842950de662247b2584b01f
Dev <dev.pahuja@verse.in>
2023-03-24 12:02:15 +0530

    throw err on delaying dls msg

--- memphis/message.py
+++ memphis/message.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 
-from memphis.exceptions import MemphisConnectError
+from memphis.exceptions import MemphisConnectError, MemphisError
 
 
 class Message:
@@ -57,25 +57,14 @@ class Message:
             return
 
     async def delay(self, delay):
-        """Delay and redeliver the message after delay seconds"""
-        try:
-            await self.message.nak(delay=delay)
-        except Exception as e:
-            if (
-                "$memphis_pm_id" in self.message.headers
-                and "$memphis_pm_sequence" in self.message.headers
-            ):
-                try:
-                    msg = {
-                        "id": self.message.headers["$memphis_pm_id"],
-                        "sequence": self.message.headers["$memphis_pm_sequence"],
-                    }
-                    msgToAck = json.dumps(msg).encode("utf-8")
-                    await self.connection.broker_manager.publish(
-                        "$memphis_pm_naks", msgToAck
-                    )
-                except Exception as er:
-                    raise MemphisConnectError(str(er)) from er
-            else:
-                raise MemphisConnectError(str(e)) from e
-            return
+        """Delay and resend the message after delay seconds"""
+        if (
+            "$memphis_pm_id" in self.message.headers
+            and "$memphis_pm_sequence" in self.message.headers
+        ):
+            raise MemphisError("cannot delay DLS message")
+        else:
+            try:
+                await self.message.nak(delay=delay)
+            except Exception as e:
+                raise MemphisError(str(e)) from e
