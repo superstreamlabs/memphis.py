29f43b1f6beb135f44e29dd883d58b00b2bc092d
idanasulinStrech <idan@memphis.dev>
2023-05-31 10:43:39 +0300

    bugfix - connection establishment

--- memphis/memphis.py
+++ memphis/memphis.py
@@ -96,6 +96,8 @@ class Memphis:
             except Exception as e:
                 if "authorization violation" in str(e).lower():
                     try:
+                        if "localhost" in connection_opts['servers']: # for handling bad quality networks like port fwd
+                            await asyncio.sleep(1)
                         ping_connection_opts["user"] = self.username
                         conn = await broker.connect(**ping_connection_opts)
                         await conn.close()
@@ -105,6 +107,8 @@ class Memphis:
                 else:
                     raise e
 
+        if "localhost" in connection_opts['servers']:
+            await asyncio.sleep(1) # for handling bad quality networks like port fwd
         return await broker.connect(**connection_opts)
 
     async def connect(
