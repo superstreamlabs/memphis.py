9b35f1d0512783c7315bc6880716f8521dcae9ab
John Peters <john@memphis.dev>
2023-12-30 11:40:21 -0600

    "touched up the formatting a little bit"

--- examples/consumer.py
+++ examples/consumer.py
@@ -1,30 +1,35 @@
-from memphis import Memphis
-from memphis.message import Message
 import asyncio
 import os
 import json
+from memphis import Memphis
+from memphis.message import Message
+
 
 async def main():
     try:
         # Connecting to the broker
         memphis = Memphis()
-    
+
         await memphis.connect(
-          host = "aws-us-east-1.cloud.memphis.dev",
-          username = "test_user",
-          password = os.environ.get("memphis_pass"),
-          account_id = os.environ.get("memphis_account_id") # For cloud users on, at the top of the overview page
-        )  
+            host="aws-us-east-1.cloud.memphis.dev",
+            username="test_user",
+            password=os.environ.get("memphis_pass"),
+            account_id=os.environ.get(
+                "memphis_account_id"
+            ),  # For cloud users on, at the top of the overview page
+        )
 
         consumer = await memphis.consumer(
             station_name="test_station",
             consumer_name="consumer",
         )
 
-        messages: list[Message] = await consumer.fetch() # Type-hint the return here for LSP integration
-        
+        messages: list[
+            Message
+        ] = await consumer.fetch()  # Type-hint the return here for LSP integration
+
         for consumed_message in messages:
-            msg_data = json.loads(consumed_message.get_data())
+            _msg_data = json.loads(consumed_message.get_data())
 
             # Do something with the message data
 
@@ -35,5 +40,6 @@ async def main():
     finally:
         await memphis.close()
 
-if __name__ == '__main__':
-  asyncio.run(main()) 
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- examples/producer.py
+++ examples/producer.py
@@ -1,38 +1,36 @@
-from memphis import Memphis
-from memphis.message import Message
 import asyncio
 import os
+from memphis import Memphis
+
 
 async def main():
     try:
         # Connecting to the broker
         memphis = Memphis()
-    
+
         await memphis.connect(
-          host = "aws-us-east-1.cloud.memphis.dev",
-          username = "test_user",
-          password = os.environ.get("memphis_pass"),
-          account_id = os.environ.get("memphis_account_id") # For cloud users on, at the top of the overview page
-        )  
+            host="aws-us-east-1.cloud.memphis.dev",
+            username="test_user",
+            password=os.environ.get("memphis_pass"),
+            account_id=os.environ.get(
+                "memphis_account_id"
+            ),  # For cloud users on, at the top of the overview page
+        )
 
         # Creating a producer and producing a message. You can also use the memphis.producer function
         producer = await memphis.producer(
-            station_name = "test_station", # Matches the station name in memphis cloud
-            producer_name = "producer"
+            station_name="test_station",  # Matches the station name in memphis cloud
+            producer_name="producer",
         )
 
         for i in range(10):
-            await producer.produce(
-                message={
-                    "id": i,
-                    "chocolates_to_eat": 3
-                }
-            )
+            await producer.produce(message={"id": i, "chocolates_to_eat": 3})
 
     except Exception as e:
         print(e)
     finally:
         await memphis.close()
 
-if __name__ == '__main__':
-  asyncio.run(main()) 
+
+if __name__ == "__main__":
+    asyncio.run(main())
