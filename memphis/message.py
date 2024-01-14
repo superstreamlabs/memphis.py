b3770083472ff782c1c2a010dc19410f4b47728c
Bazen <49089563+bazen-teklehaymanot@users.noreply.github.com>
2024-01-14 14:29:39 +0200

    Added attribute check on message

--- memphis/message.py
+++ memphis/message.py
@@ -42,6 +42,8 @@ class Message:
         """
         nack - not ack for a message, meaning that the message will be redelivered again to the same consumers group without waiting to its ack wait time.
         """
+        if not hasattr(self.message, 'nak'):
+            return
         await self.message.nak()
 
     async def dead_letter(self, reason: str):
@@ -50,6 +52,8 @@ class Message:
         The message will still be available to other consumer groups
         """
         try:
+            if not hasattr(self.message, 'term'):
+                return
             await self.message.term()
             md = self.message.metadata()
             stream_seq = md.sequence.stream
